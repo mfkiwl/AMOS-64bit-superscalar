3c8e9630bb477b7af27231d166a43973f9a8d53f
Fabian Schuiki <fabian@schuiki.ch>
2018-07-11 22:09:17 +0200

    Add empty decls of channel etc.
    Add declarations for `Channel`, `ChannelDatum`, and `Cycle` to the sim
    library. These don't do anything yet, but provide a rough sketch on how
    things should be used. We'll add some form of lock-free queues as the
    actual implementation of the FIFO underneath, e.g. [1].
    
    [1]: http://moodycamel.com/blog/2014/a-fast-general-purpose-lock-free-queue-for-c++

--- Makefile
+++ Makefile
@@ -2,15 +2,17 @@ TARGET_EXEC ?= amos_tb
 
 BUILD_DIR ?= ./build
 SRC_DIRS ?= ./csrc
+INC_DIRS ?= ./include
 RTL_DIR ?= ./rtl
 
 VERILATOR ?= verilator
 
 SRCS := $(shell find $(SRC_DIRS) -name *.cpp -or -name *.c -or -name *.s)
+HDRS := $(shell find $(SRC_DIRS) $(INC_DIRS) -name *.hpp -or -name *.h)
 OBJS := $(SRCS:%=$(BUILD_DIR)/%.o)
 DEPS := $(OBJS:.o=.d)
 
-INC_DIRS := $(shell find $(SRC_DIRS) -type d) $(VERILATOR_ROOT)/include ./include
+INC_DIRS := $(shell find $(SRC_DIRS) -type d) $(VERILATOR_ROOT)/include $(INC_DIRS)
 INC_FLAGS := $(addprefix -I,$(INC_DIRS))
 
 CPPFLAGS ?= $(INC_FLAGS) -pthread -Wall -std=c++11
@@ -23,17 +25,17 @@ $(BUILD_DIR)/$(TARGET_EXEC): $(OBJS)
 	$(CXX) $(OBJS) -o $@ $(LDFLAGS)
 
 # assembly
-$(BUILD_DIR)/%.s.o: %.s
+$(BUILD_DIR)/%.s.o: %.s $(HDRS)
 	$(MKDIR_P) $(dir $@)
 	$(AS) $(ASFLAGS) -c $< -o $@
 
 # c source
-$(BUILD_DIR)/%.c.o: %.c
+$(BUILD_DIR)/%.c.o: %.c $(HDRS)
 	$(MKDIR_P) $(dir $@)
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
 
 # c++ source
-$(BUILD_DIR)/%.cpp.o: %.cpp
+$(BUILD_DIR)/%.cpp.o: %.cpp $(HDRS)
 	$(MKDIR_P) $(dir $@)
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $< -o $@
 
--- include/simlib.hpp
+++ include/simlib.hpp
@@ -1,3 +1,54 @@
 // Copyright (c) 2018 AMOS Developers
 #pragma once
+#include <cstdint>
+#include <cassert>
 
+namespace simlib {
+
+/// A number of clock cycles.
+///
+/// Use this type whenever it is necessary to track the number of clock cycles
+/// that have passed.
+typedef uint64_t Cycle;
+
+/// An item passed through a channel, together with a cycle count.
+///
+/// The cycle count is used to implicitly keep track of clock cycles.
+template <typename T>
+struct ChannelDatum {
+	typedef T Item;
+	Item item;
+	Cycle cycle;
+
+	ChannelDatum(Item item, Cycle cycle) : item(item), cycle(cycle) {}
+};
+
+/// A FIFO channel
+///
+/// A channel emulates a first-in first-out means of communication between
+/// different modules. Its semantics are the same as an actual FIFO in hardware,
+/// but it serves a double-purpose as primitive to support multi-threaded
+/// simulation.
+template <typename T>
+struct Channel {
+	typedef T Item;
+	typedef ChannelDatum<Item> Datum;
+
+	/// Push an item into the FIFO.
+	///
+	/// If the FIFO is full, blocks until space becomes available.
+	template <typename... Args> void
+	push (Args... args) {
+		assert(false && "not implemented");
+	}
+
+	/// Pop an item from the FIFO.
+	///
+	/// If the FIFO is empty, blocks until an item is available.
+	Datum
+	pop () {
+		assert(false && "not implemented");
+	}
+};
+
+} // namespace simlib
