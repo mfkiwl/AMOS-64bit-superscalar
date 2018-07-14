95c6679be56261d22c0a7641155244687aca50d3
Fabian Schuiki <fabian@schuiki.ch>
2018-07-14 16:49:04 +0200

    Add simple two stage simulation engine
    Add a very simple simulation engine based on a ping-pong scheme.
    Multiple threads update all components in the system, which are isolated
    from each other. They then run into a barrier, after which all channels
    will be activated which transport their data items from the sending to
    the receiving side. This emulates a clock cycle. Then another barrier
    is hit, which resynchronizes all worker threads and the process repeats.
    
    Various optimizations are possible in the future, regarding cache and
    snooping friendliness by laying out the channel slots in memory
    properly. Only single-threaded single-stepping implemented at the
    moment.

--- csrc/simlib-test.cpp
+++ csrc/simlib-test.cpp
@@ -2,8 +2,54 @@
 #include <simlib.hpp>
 #include <iostream>
 
+struct Producer {
+	ChannelTx<uint64_t> out;
+	uint64_t counter;
+
+	void update() {
+		if (out) {
+			out = counter++;
+		}
+	}
+};
+
+struct Consumer  {
+	ChannelRx<uint64_t> in;
+
+	void update() {
+		if (in) {
+			std::cout << "consumed " << in.pop() << "\n";
+		}
+	}
+};
+
+struct Mangler {
+	ChannelRx<uint64_t> in;
+	ChannelTx<uint64_t> out;
+
+	void update() {
+		if (in && out) {
+			auto v = in.pop();
+			out = v ^ (v << 13);
+		}
+	}
+};
+
 int
 main (int argc, char **argv) {
-	std::cout << "Hello from simlib\n";
+
+	Simulation sim;
+	Builder builder(&sim);
+	auto c0 = builder.make_channel<uint64_t>();
+	auto c1 = builder.make_channel<uint64_t>();
+	auto c2 = builder.make_channel<uint64_t>();
+	builder.add_component(Producer { .out = c0.tx, .counter = 0 });
+	builder.add_component(Mangler { .in = c0.rx, .out = c1.tx });
+	builder.add_component(Mangler { .in = c1.rx, .out = c2.tx });
+	builder.add_component(Consumer { .in = c2.rx });
+
+	for (int i = 0; i < 10; i++)
+		sim.step();
+
 	return 0;
 }
--- include/simlib.hpp
+++ include/simlib.hpp
@@ -2,8 +2,8 @@
 #pragma once
 #include <cstdint>
 #include <cassert>
-
-namespace simlib {
+#include <memory>
+#include <vector>
 
 /// A number of clock cycles.
 ///
@@ -11,44 +11,196 @@ namespace simlib {
 /// that have passed.
 typedef uint64_t Cycle;
 
-/// An item passed through a channel, together with a cycle count.
+/// A basic channel slot.
 ///
-/// The cycle count is used to implicitly keep track of clock cycles.
-template <typename T>
-struct ChannelDatum {
-	typedef T Item;
-	Item item;
-	Cycle cycle;
+/// Concrete channel slots inherit from this base class and override the virtual
+/// functions with concrete implementations according to the contained type.
+struct AbstractChannelSlot {
+	virtual void transport() = 0;
 
-	ChannelDatum(Item item, Cycle cycle) : item(item), cycle(cycle) {}
+	bool tx_valid = false;
+	bool tx_ready = true;
+	bool rx_valid = false;
+	bool rx_ready = true;
 };
 
-/// A FIFO channel
+/// A channel slot allocated in the simulation core.
+template <typename T>
+struct ChannelSlot : public AbstractChannelSlot {
+	T tx_datum;
+	T rx_datum;
+
+	void transport() override {
+		if (tx_valid && rx_ready) {
+			tx_valid = false;
+			tx_ready = true;
+			rx_valid = true;
+			rx_ready = false;
+			std::swap(tx_datum, rx_datum);
+		}
+	}
+};
+
+/// A hardware component.
 ///
-/// A channel emulates a first-in first-out means of communication between
-/// different modules. Its semantics are the same as an actual FIFO in hardware,
-/// but it serves a double-purpose as primitive to support multi-threaded
-/// simulation.
+/// Behaviour and computation is represented as classes that implement this
+/// interface. The simulation assumes ownership of all components and triggers
+/// updates as appropriate.
+struct AbstractComponent {
+	virtual void update() = 0;
+};
+
+template <typename T>
+struct Component : public AbstractComponent {
+	T wrapped;
+
+	Component(T wrapped) : wrapped(wrapped) {}
+
+	void
+	update() override {
+		wrapped.update();
+	}
+};
+
+/// A simulation core.
+class Simulation {
+public:
+	void
+	step () {
+		// Simulate components.
+		for (const auto &c : components) {
+			c->update();
+		}
+
+		// Transport data.
+		for (const auto &c : channels) {
+			c->transport();
+		}
+	}
+
+protected:
+	friend class Builder;
+
+	/// All channels in the simulation.
+	std::vector<std::unique_ptr<AbstractChannelSlot>> channels;
+
+	/// All components in the simulation.
+	std::vector<std::unique_ptr<AbstractComponent>> components;
+};
+
+/// A sending end of a channel.
+template <typename T>
+class ChannelTx {
+protected:
+	friend struct Builder;
+
+	ChannelSlot<T> *slot;
+	ChannelTx(ChannelSlot<T> *slot) : slot(slot) {}
+
+public:
+	/// Check whether the channel is ready to accept data.
+	inline bool
+	ready () const {
+		return slot->tx_ready;
+	}
+
+	inline
+	operator bool () const {
+		return ready();
+	}
+
+	/// Push a value into the channel to be moved at the next clock edge.
+	inline void
+	push (T datum) const {
+		slot->tx_datum = datum;
+		slot->tx_valid = true;
+	}
+
+	inline void
+	operator = (T datum) const {
+		push(datum);
+	}
+};
+
+/// A receiving end of a channel.
+template <typename T>
+class ChannelRx {
+protected:
+	friend struct Builder;
+
+	ChannelSlot<T> *slot;
+	ChannelRx(ChannelSlot<T> *slot) : slot(slot) {}
+
+public:
+	/// Check whether the channel contains data.
+	inline bool
+	valid () const {
+		return slot->rx_valid;
+	}
+
+	inline
+	operator bool () const {
+		return valid();
+	}
+
+	/// Pop a value from the channel.
+	inline T
+	pop () const {
+		slot->rx_ready = true;
+		return slot->rx_datum;
+	}
+
+	inline
+	operator T () const {
+		return pop();
+	}
+};
+
+/// A communication channel pair.
+///
+/// Wraps around the sender and receiver resources allocated in the simulation
+/// core that may be used to transport data each clock cycle.
 template <typename T>
 struct Channel {
-	typedef T Item;
-	typedef ChannelDatum<Item> Datum;
-
-	/// Push an item into the FIFO.
-	///
-	/// If the FIFO is full, blocks until space becomes available.
-	template <typename... Args> void
-	push (Args... args) {
-		assert(false && "not implemented");
-	}
-
-	/// Pop an item from the FIFO.
-	///
-	/// If the FIFO is empty, blocks until an item is available.
-	Datum
-	pop () {
-		assert(false && "not implemented");
-	}
+	/// The sending part of the channel.
+	ChannelTx<T> tx;
+	/// the receiving part of the channel.
+	ChannelRx<T> rx;
 };
 
-} // namespace simlib
+/// A helper class to prepare the simulation.
+///
+/// The builder class is used as a helper to assemble the simulation hierarchy
+/// and associated resources. It wraps around a simulation core and allocates
+/// resources as more modules and channels are added.
+class Builder {
+	/// The simulation the builder constructs.
+	Simulation *sim;
+
+public:
+	Builder(Simulation *sim) : sim(sim) {}
+
+	/// Create a communication channel pair.
+	template <typename T> Channel<T>
+	make_channel () {
+		std::unique_ptr<ChannelSlot<T>> ptr(new ChannelSlot<T>());
+		Channel<T> chan {
+			.tx = ChannelTx<T>(ptr.get()),
+			.rx = ChannelRx<T>(ptr.get())
+		};
+		sim->channels.push_back(std::move(ptr));
+		return chan;
+	}
+
+	/// Add a component to the simulation.
+	///
+	/// The simulation allocates memory for the component and assumes ownership.
+	/// Returns a pointer to the final component, which is usually not needed.
+	template <typename T> inline T *
+	add_component(T component) {
+		std::unique_ptr<Component<T>> ptr(new Component<T>(component));
+		auto p = &ptr->wrapped;
+		sim->components.push_back(std::move(ptr));
+		return p;
+	}
+};
